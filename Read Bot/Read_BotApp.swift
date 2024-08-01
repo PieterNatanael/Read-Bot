d6476e39952d7465e3feeacad3cbadb4ecdce771
PieterNatanael <pieternatanael@icloud.com>
2024-08-01 17:54:35 +0800

    Initial Commit

---
+++ Read Bot/Read_BotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Read_BotApp.swift
+//  Read Bot
+//
+//  Created by Pieter Yoshua Natanael on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Read_BotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
